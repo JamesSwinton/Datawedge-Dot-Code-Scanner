f643626fcc4807fe488f65cc273fb3cbb1b72628
JS6845 <JS6845@zebra.com>
2019-03-05 14:44:14 +0000

    Initial Commit

---
+++ app/src/main/java/jamesswinton/com/zebra/dotcodescanner/ContinuousScanActivity.java
@@ -0,0 +1,13 @@
+package jamesswinton.com.zebra.dotcodescanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContinuousScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_continuous_scan);
+    }
+}
