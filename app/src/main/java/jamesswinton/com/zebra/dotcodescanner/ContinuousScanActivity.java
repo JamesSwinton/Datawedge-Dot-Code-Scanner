7eb8de46ae5236204cfd3da667b71256696846c4
JS6845 <JS6845@zebra.com>
2019-03-05 15:33:00 +0000

    Implemented DataWedge Broadcast Receiver

--- app/src/main/java/jamesswinton/com/zebra/dotcodescanner/ContinuousScanActivity.java
+++ app/src/main/java/jamesswinton/com/zebra/dotcodescanner/ContinuousScanActivity.java
@@ -1,5 +1,7 @@
 package jamesswinton.com.zebra.dotcodescanner;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,11 +19,44 @@ public class ContinuousScanActivity extends AppCompatActivity {
     // Variables
     private ActivityContinuousScanBinding mDataBinding;
 
+    private IntentFilter mDataWedgeIntentFilter;
+    private DataWedgeReceiver mDataWedgeReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Init DataBinding
         mDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_continuous_scan);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Create Intent Filter
+        if (mDataWedgeIntentFilter == null) {
+            mDataWedgeIntentFilter = new IntentFilter();
+            mDataWedgeIntentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+            mDataWedgeIntentFilter.addAction(getResources().getString(R.string.scan_intent_action));
+        }
+
+        // Create Instance of Broadcast Receiver
+        if (mDataWedgeReceiver == null) {
+            mDataWedgeReceiver = new DataWedgeReceiver();
+        }
+
+        // Register Receiver
+        registerReceiver(mDataWedgeReceiver, mDataWedgeIntentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Un-Register Receiver
+        if (mDataWedgeReceiver != null) {
+            unregisterReceiver(mDataWedgeReceiver);
+        }
     }
 }
