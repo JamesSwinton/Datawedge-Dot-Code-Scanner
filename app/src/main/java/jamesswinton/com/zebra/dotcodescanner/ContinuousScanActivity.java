236a6fa1b64df092478a477e2c0bc90f2fc7bd34
JS6845 <JS6845@zebra.com>
2019-03-05 15:26:51 +0000

    Implemented DataBinding

--- app/src/main/java/jamesswinton/com/zebra/dotcodescanner/ContinuousScanActivity.java
+++ app/src/main/java/jamesswinton/com/zebra/dotcodescanner/ContinuousScanActivity.java
@@ -1,13 +1,27 @@
 package jamesswinton.com.zebra.dotcodescanner;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import jamesswinton.com.zebra.dotcodescanner.databinding.ActivityContinuousScanBinding;
+
 public class ContinuousScanActivity extends AppCompatActivity {
+
+    // Debugging
+    private static final String TAG = "ContinuousScanActivity";
+
+    // Constants
+
+
+    // Variables
+    private ActivityContinuousScanBinding mDataBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_continuous_scan);
+
+        // Init DataBinding
+        mDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_continuous_scan);
     }
 }
