92f0f86eb5ad2f1021d9065aff61e51a8929244a
JS6845 <JS6845@zebra.com>
2019-03-06 11:17:02 +0000

    Created constants for Datawedge APIs

--- app/src/main/java/jamesswinton/com/zebra/dotcodescanner/ContinuousScanActivity.java
+++ app/src/main/java/jamesswinton/com/zebra/dotcodescanner/ContinuousScanActivity.java
@@ -7,7 +7,13 @@ import android.content.IntentFilter;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import jamesswinton.com.zebra.dotcodescanner.databinding.ActivityContinuousScanBinding;
 
@@ -16,12 +22,31 @@ public class ContinuousScanActivity extends AppCompatActivity {
     // Debugging
     private static final String TAG = "ContinuousScanActivity";
 
-    // Constants
+    /*
+     * Constants
+     */
 
+    // Actions
+    private static final String DATAWEDGE_ACTION = "com.symbol.datawedge.api.ACTION";
+    private static final String NOTIFICATION_ACTION  = "com.symbol.datawedge.api.NOTIFICATION_ACTION";
+
+    // Triggers
+    private static final String DW_NOTIFICATION_TYPE = "com.symbol.datawedge.api.NOTIFICATION_TYPE";
+    private static final String APPLICATION_NAME = "com.symbol.datawedge.api.APPLICATION_NAME";
+    private static final String NOTIFICATION = "com.symbol.datawedge.api.NOTIFICATION";
+    private static final String SOFT_SCAN_TRIGGER = "com.symbol.datawedge.api.SOFT_SCAN_TRIGGER";
+    private static final String REGISTER_FOR_NOTIFICATION = "com.symbol.datawedge.api.REGISTER_FOR_NOTIFICATION";
+
+    // Parameters
+    private static final String STATUS = "STATUS";
+    private static final String STOP_SCANNING = "STOP_SCANNING";
+    private static final String START_SCANNING = "START_SCANNING";
+    private static final String SCANNER_STATUS = "SCANNER_STATUS";
+    private static final String TOGGLE_SCANNING = "TOGGLE_SCANNING";
+    private static final String NOTIFICATION_TYPE = "NOTIFICATION_TYPE";
 
     // Variables
     private ActivityContinuousScanBinding mDataBinding;
-
     private IntentFilter mDataWedgeIntentFilter;
     private BroadcastReceiver mDataWedgeReceiver;
 
